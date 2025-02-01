99c0c6c10efd8292a54081063ceb38f8a71241f9
shanta1996 <shantachaudhary490@gmail.com>
2025-02-01 18:23:12 +0545

    changes

---
+++ STRINGS/MATH/math.js
@@ -0,0 +1,20 @@
+// const obj1={
+//     name:'shanta',
+//     thar:'chaudhary'
+// }
+// console.log(obj1.name)
+
+// const obj2={}
+// Object.assign(obj2,obj1)
+// const obj3={...obj2}
+
+
+// debugger
+// var a='name'
+// let b='chaudhary'
+// console.log(a,b)
+
+// function abc(){
+// console.log('hello')
+// }
+
--- STRINGS/strings.js
+++ STRINGS/strings.js
@@ -1,5 +1,12 @@
-const letters='   hello there    ohoo'
-const ltr=letters.toLocaleLowerCase().trim()
-const name=`my name is ${letters}`
+// const letters='   hello there    ohoo'
+// const ltr=letters.toLocaleLowerCase().trim()
+// const name=`my name is ${letters}`
 
+const heading=document.querySelectorAll('h1')
 
+// heading[0].style.color='teal'
+// heading[1].style.backgroundColor='teal'
+
+for(let i=0;i<heading.length;i++){
+    heading[i].style.color='red'
+}
