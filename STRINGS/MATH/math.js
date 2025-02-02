4a3b27582ed966eac775980564ec75541ed170ea
shanta1996 <shantachaudhary490@gmail.com>
2025-02-02 21:44:59 +0545

    commit

--- STRINGS/MATH/math.js
+++ STRINGS/MATH/math.js
@@ -28,5 +28,26 @@ for (let i=1;i<=10;i++){
     </div>`
 }
 container.innerHTML=myhtml
-container.remove()
+// container.remove()
 // body.append(container)
+
+const body=document.querySelector('body')
+const button=document.querySelector('button')
+const handleClick=()=>{
+body.style.backgroundColor='black'
+}
+
+
+const heading=document.querySelector('h1')
+const p=document.querySelector('.p')
+let count=1
+heading.addEventListener('click',()=>{
+    const newElem=document.createElement('h2')
+    newElem.classList.add('newlem')
+    // newElem.innerText='hello world...'
+    newElem.innerText=`${count} hello world${count}`
+    count++
+    body.append(newElem)
+    newElem.style.color='blue'
+    // p.innerHTML=`<h1>hello world</h1>`
+})
