cbd696667d808b79041a733b778e4e6adfe9f459
shanta1996 <shantachaudhary490@gmail.com>
2025-02-06 14:42:09 +0545

    changes

---
+++ mydata.js
@@ -0,0 +1,6 @@
+const user = {
+  userId: 1,
+  id: 1,
+  title: "delectus aut autem",
+  completed: false,
+};
---
+++ optionalChaining.js
@@ -0,0 +1,5 @@
+const user = {
+  name: "shanta",
+  thr: "chaudhary",
+};
+console.log(user.address?.city);
